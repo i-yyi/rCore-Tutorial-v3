96f1e07572faa06d5c742b05d46e7626e90873cf
i-yyi <i-yyi@users.noreply.github.com>
2024-07-29 10:15:20 +0000

    deploy: ae67ed7214b650a5bed8167a8f706702eaefbcee

--- yyi/ch1/os/sidebar-items.js
+++ yyi/ch1/os/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["log","print","println"],"mod":["console","lang_items","sbi"]};
+window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["DEBUG","ERROR","INFO","LOG","TRACE","WARN","print","println"],"mod":["console","lang_items","log","sbi"]};
--- yyi/ch1/src-files.js
+++ yyi/ch1/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["os",["",[],["console.rs","lang_items.rs","main.rs","sbi.rs"]]]\
+["os",["",[],["console.rs","lang_items.rs","log.rs","main.rs","sbi.rs"]]]\
 ]'));
 createSrcSidebar();
